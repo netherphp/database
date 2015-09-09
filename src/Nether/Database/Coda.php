fb08c80cad29665a23eafc51ecdf8700ab2c0baf
Bob Majdak Jr <bob@dreamcars.com>
2015-09-09 02:02:36 -0500

    clean up the coda class

--- src/Nether/Database/Coda.php
+++ src/Nether/Database/Coda.php
@@ -3,10 +3,49 @@
 namespace Nether\Database;
 use \Nether;
 
+use \Exception;
+
 abstract class Coda {
+/*//
+co-da /ˈkōdə/
+> the concluding passage of a piece or movement, typically forming an
+> addition to the basic structure.
+
+codas allow you to write some annoying or tricky things in SQL without having
+to actually write the SQL to do it. the best example is proably for writing
+an IN() clause, which PDO on its own has no ability to bind arrays while
+binding parameters. see the Coda\In file for more detailed example.
+
+this class provides the framework interface and some default implementation
+for how codas work. each coda typically will only work upon one small addition
+to a query, thusly only operating upon one field.
+
+each coda must know at least three things before they attempt to render.
+
+1) they must have a hold on a database connection so that they may properly
+   escape data using the charset and all that jazz.
+2) they must know what field you intend to operate on.
+3) they must know the value or values you wish to operate with.
+
+this data may be added to the coda at any order and at any time as long as
+it knows all three things before you try and print or render it. so for example
+you can write a search api method that takes codas as arguments that have only
+their values yet. your method could then append the database and field name to
+it afterwards.
+
+it is up to the author of the coda to ensure what he allows you to do cannot
+get you sql injected. this is why we need access to the connection, so that
+anything can be manually escaped if the coda is building a complex value.
+//*/
 
 	public function
 	__construct($opt=null) {
+	/*//
+	@argv object|array
+	nothing special happens during construction. you can pass an array that
+	contains a database connection, the field name, or the value. all of which
+	can be specified at a later time.
+	//*/
 
 		$opt = new Nether\Object($opt,[
 			'Database' => null,
@@ -23,55 +62,137 @@ abstract class Coda {
 
 	public function
 	__toString() {
+	/*//
+	@return string
+	have the coda automatically build and render when you attept to use it in a
+	string context.
+	//*/
+
 		return $this->Render();
 	}
 
-	////////////////
-	////////////////
+	////////////////////////////////
+	////////////////////////////////
 
-	protected $Database;
-
-	public function
-	GetDatabase() { return $this->Database; }
-
-	public function
-	SetDatabase(Nether\Database $DB) { $this->Database = $DB; return $this; }
-
-	protected $Field;
+	protected
+	$Database = null;
+	/*//
+	@type Nether\Database
+	the db connection to use for things like escaping data.
+	//*/
 
 	public function
-	GetField() { return $this->Field; }
+	GetDatabase() {
+	/*//
+	@return Nether\Database | null
+	return the db connection or null if one has not yet been set.
+	//*/
+
+		return $this->Database;
+	}
 
 	public function
-	SetField($Field) { $this->Field = $Field; return $this; }
+	SetDatabase(Nether\Database $DB) {
+	/*//
+	@argv Nether\Database
+	@return self
+	set a db connection for this coda.
+	//*/
 
-	protected $Value;
+		$this->Database = $DB;
+		return $this;
+	}
+
+	////////////////////////////////
+	////////////////////////////////
+
+	protected
+	$Field = null;
+	/*//
+	@type string
+	the database field that this coda is for.
+	//*/
 
 	public function
-	GetValue() { return $this->Value; }
+	GetField() {
+	/*//
+	@return string | null
+	get the database field this coda is for or null if it has not yet been set.
+	//*/
+
+		return $this->Field;
+	}
 
 	public function
-	SetValue($Value) { $this->Value = $Value; return $this; }
+	SetField($Field) {
+	/*//
+	@argv string
+	@return self
+	set the field this coda is for.
+	//*/
 
-	////////////////
-	////////////////
+		$this->Field = $Field;
+		return $this;
+	}
+
+	////////////////////////////////
+	////////////////////////////////
+
+	protected
+	$Value = null;
+	/*//
+	@type mixed
+	the value to use in the coda.
+	//*/
+
+	public function
+	GetValue() {
+	/*//
+	@return string | null
+	get the value to be used in this coda or null if nothing has been set yet.
+	//*/
+
+		return $this->Value;
+	}
+
+	public function
+	SetValue($Value) {
+	/*//
+	@argv mixed
+	set the value to use in this coda.
+	//*/
+
+		$this->Value = $Value;
+		return $this;
+	}
+
+	////////////////////////////////
+	////////////////////////////////
 
 	protected final function
 	RequireDatabase() {
 	/*//
-	demand a database to have been set for things like rendering.
+	demand a database to have been set for things like rendering. i know that
+	when you throw this php will be all like "tostring cant throw excptions you
+	idiot" and then refuse to show you this actual error. but whatever, we need
+	it to die somehow if you forgot to give it a db.
 	//*/
 
-		//if(!$this->Database)
-		//throw new \Exception("No database has been defined for this Coda to use for sanitisation yet.");
+		if(!$this->Database)
+		throw new Exception("No database has been defined for this Coda.");
 
-		return;		
+		return;
 	}
+
+	////////////////////////////////
+	////////////////////////////////
 
 	public function
 	Render() {
 	/*//
-	compile this coda down into sql
+	render this coda down to its desired SQL component. this implementaiton
+	will allow you to define new methods in your extension suffixed with
+	the pdo driver name so that your coda may support multiple databases.
 	//*/
 
 		$this->RequireDatabase();
