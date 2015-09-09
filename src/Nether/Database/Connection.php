9ef2df04f36f0fe2efa3416585623af6dd100984
Bob Majdak Jr <bob@dreamcars.com>
2015-09-09 03:25:46 -0500

    cleanup of the connection class

--- src/Nether/Database/Connection.php
+++ src/Nether/Database/Connection.php
@@ -7,81 +7,101 @@ use \Nether;
 ////////////////
 
 class Connection {
+/*//
+this class defines the configuration values required to connect to the various
+database servers you may setup in your site config.
+//*/
 
-	public $Type;
+	public
+	$Type = null;
 	/*//
 	@type string
-	the database server type we are connecting to. it should be a type
-	PDO understands.
+	the database server type we are connecting to. it should be a type PDO
+	understands as one of its available drivers.
 	//*/
 
-	public $Hostname;
+	public
+	$Hostname = null;
 	/*//
 	@type string
 	the hostname of the server we are connecting to.
 	//*/
 
-	public $Database;
+	public
+	$Database = null;
 	/*//
 	@type string
 	the database name for the database connection.
 	//*/
 
-	public $Username;
+	public
+	$Username = null;
 	/*//
 	@type string
 	the username required for the database connection.
 	//*/
 
-	public $Password;
+	public
+	$Password = null;
 	/*//
 	@type string
 	the password required for the database connection.
 	//*/
 
-	public function __construct($opt) {
-		$opt = new Nether\Object($opt,[
+	////////////////////////////////
+	////////////////////////////////
+
+	public function
+	__construct($Opt) {
+	/*//
+	@argv object|array
+	//*/
+
+		$Opt = new Nether\Object($Opt,[
 			'Type'     => null,
 			'Hostname' => null,
 			'Username' => null,
 			'Password' => null,
 			'Database' => null
-		]);
+		],['DefaultKeysOnly'=>true]);
 
-		foreach($opt as $prop => $val)
-		$this->{$prop} = $val;
+		foreach($Opt as $Prop => $Val)
+		$this->{$Prop} = $Val;
 
 		return;
 	}
 
-	public function __toString() {
+	public function
+	__toString() {
+	/*//
+	when used in a string context dump out the pdo connection string.
+	//*/
+
 		return $this->GetDSN();
 	}
 
-	////////////////
-	////////////////
+	////////////////////////////////
+	////////////////////////////////
 
-	public function GetDSN() {
+	public function
+	GetDSN() {
 	/*//
 	@type string
 	get the connection dsn string for this database connection.
 	//*/
 
 		switch($this->Type) {
-
 			case 'mysql': return sprintf(
 				'%s:host=%s;dbname=%s',
 				$this->Type,
 				$this->Hostname,
 				$this->Database
 			);
-
 			case 'sqlite': return sprintf(
 				'%s:%s',
 				$this->Type,
 				$this->Database
 			);
-
 			default: return sprintf(
 				'%s:host=%s;dbname=%s, %s, %s',
 				$this->Type,
@@ -90,8 +110,8 @@ class Connection {
 				$this->Username,
 				$this->Password
 			);
-
 		}
 	}
+
 
 }
