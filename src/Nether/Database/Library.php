8dbba143dba7f35941f469ce29f94acd0d95f087
bobmagicii <bob@majdak.net>
2022-11-09 01:02:09 +0000

    library api for new atlantis

---
+++ src/Nether/Database/Library.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Nether\Database;
+use Nether;
+
+use Nether\Object\Datastore;
+
+class Library {
+
+	static public function
+	Init(string $Path, string $Env, ...$Argv):
+	void {
+
+		$Filename = sprintf(
+			'%s/conf/env/%s/netherdb.json',
+			$Path,
+			$Env
+		);
+
+		if(file_exists($Filename) && is_readable($Filename))
+		Nether\Database\ConnectionConfig::LoadFromJSON($Filename);
+
+		return;
+	}
+
+}
